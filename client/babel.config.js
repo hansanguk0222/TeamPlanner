b7203fcfb89567cc9064cd944d34dc924889c051
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-08 11:39:20 +0900

    [feat] 바벨 수정

---
+++ client/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: '> 0.25%, not dead',
+        useBuiltIns: 'usage',
+        corejs: '3',
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ];
+  const plugins = [
+    ['@babel/plugin-transform-async-to-generator'],
+    ['@babel/plugin-transform-runtime'],
+  ];
+
+  return {
+    plugins,
+    presets,
+  };
+};
