2597b9b6e38abfdb3cfd0cfc80df5a5fcfa6805f
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-13 19:04:36 +0900

    [feat] 리액트 라우터로 URL이 변경되면 브라우저에 나타는 컴포넌트가 변경될 수 있도록 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '@/styles/theme';
-import LoginBox from '@/components/LoginBox/LoginBox';
+import { LoginPage, SignUpPage } from '@/pages';
 import { GlobalStyle } from '@/styles/';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <LoginBox />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/join" component={SignUpPage} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
