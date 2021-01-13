bf34eb949758a7596a2eb4b2b70dbc1f86be344f
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-13 19:03:40 +0900

    [refactor] return을 사용하지 않고 바로 반환하게 만듬

--- client/src/store/actions/auth.action.ts
+++ client/src/store/actions/auth.action.ts
@@ -4,24 +4,17 @@ export const LOGIN_REQUEST = 'auth/LOGIN_REQUEST' as const;
 export const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS' as const;
 export const LOGIN_ERROR = 'auth/LOGIN_ERROR' as const;
 
-export const loginRequest = ({ email, pw }: LoginPayload) => {
-  console.log('여긴가');
-  return {
-    type: LOGIN_REQUEST,
-    payload: { email, pw },
-  };
-};
+export const loginRequest = ({ email, pw }: LoginPayload) => ({
+  type: LOGIN_REQUEST,
+  payload: { email, pw },
+});
 
-export const loginSuccess = ({ accessToken, refreshToken }: LoginSuccessPayload) => {
-  return {
-    type: LOGIN_SUCCESS,
-    payload: { accessToken, refreshToken },
-  };
-};
+export const loginSuccess = ({ accessToken, refreshToken }: LoginSuccessPayload) => ({
+  type: LOGIN_SUCCESS,
+  payload: { accessToken, refreshToken },
+});
 
-export const loginError = ({ err }: LoginErrorPayload) => {
-  return {
-    type: LOGIN_ERROR,
-    payload: { err },
-  };
-};
+export const loginError = ({ err }: LoginErrorPayload) => ({
+  type: LOGIN_ERROR,
+  payload: { err },
+});
