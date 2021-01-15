3f688974f15cd102ba59d54cdf2ae75464b961d2
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:51:22 +0900

    [feat] accessToken인지 refreshToken인지 구분하는 타입을 추가

--- client/src/utils/contants.ts
+++ client/src/utils/contants.ts
@@ -2,3 +2,8 @@ export const ERROR_MESSAGE = {
   NO_EXIST_USER: '아이디나 비밀번호를 확인해주세요',
   LOGIN_ERROR: '로그인 에러가 발생하였습니다. 다시 시도해주세요',
 };
+
+export const TOKEN_TYPE = {
+  ACCESS: 'ACCESS' as const,
+  REFRESH: 'REFRESH' as const,
+};
