2bcceaca87b070964c189dc1f74eed3f4b81f74d
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:50:36 +0900

    [feat] api 주소 변경

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -4,11 +4,11 @@ export const authService = {
   login({ email, pw }: { email: string; pw: string }) {
     return axios.post('/api/auth/login', { email, pw });
   },
-  overlap({ email }: { email: string }) {
-    return axios.post('/api/auth/email/existance', { email });
+  overlapEmail({ email }: { email: string }) {
+    return axios.post('/api/auth/email/join', { email });
   },
   join({ email, pw, nickname }: { email: string; pw: string; nickname: string }) {
-    return axios.post('/api/auth/sign-up', { email, pw, nickname });
+    return axios.post('/api/auth/join', { email, pw, nickname });
   },
   authorizeEmail({ email }: { email: string }) {
     return axios.post('/api/auth/email/code', { email });
--- server/src/routes/api/auth/index.ts
+++ server/src/routes/api/auth/index.ts
@@ -4,6 +4,7 @@ import * as authRouter from './auth.controller';
 const router = express.Router();
 
 router.post('/login', authRouter.userLogin);
-router.post('/sign-up', authRouter.userJoin);
-
+router.post('/join', authRouter.userJoin);
+router.post('/email/join', authRouter.isExistUser);
+router.post('/email/code', authRouter.sendEmail);
 export default router;
