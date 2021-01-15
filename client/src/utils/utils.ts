00a96e6c971e46690f306895dee1a2e694849cd1
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:54:08 +0900

    [feat] 회원가입에서 사용할 이메일 인증과 비밀번호 인증, 로그인에서 사용할 이메일 인증과 비밀번호 인증을 따로 구분하고 토큰이 유효한지 검사하는 함수를 추가

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,4 +1,15 @@
-export const isValidEmail = ({ setEmailValidCheck, email }: { setEmailValidCheck: (emailValidCheck: boolean) => void; email: string }) => {
+import jwt from 'jsonwebtoken';
+import config from '@/config';
+import { TOKEN_TYPE } from '@/utils/contants';
+import { AuthToken } from '@/types';
+
+export const isValidLoginEmail = ({
+  setEmailValidCheck,
+  email,
+}: {
+  setEmailValidCheck: (emailValidCheck: boolean) => void;
+  email: string;
+}): boolean => {
   const regex = /^[0-9a-zA-Z]*\@[0-9a-zA-Z]*\.(com|net|co.kr)$/i;
   if (email !== '' && regex.test(email)) {
     setEmailValidCheck(true);
@@ -8,7 +19,51 @@ export const isValidEmail = ({ setEmailValidCheck, email }: { setEmailValidCheck
   return false;
 };
 
-export const isValidPw = ({ setPwValidCheck, pw }: { setPwValidCheck: (pwValidCheck: boolean) => void; pw: string }) => {
+export const isValidSignUpEmail = ({
+  setEmailValidCheck,
+  setEmailCode,
+  setEmailCodeSameCheck,
+  email,
+}: {
+  setEmailValidCheck: (emailValidCheck: boolean) => void;
+  setEmailCode: (emailCode: string) => void;
+  setEmailCodeSameCheck: (emailcodeSameCheck: boolean) => void;
+  email: string;
+}): boolean => {
+  setEmailCode('');
+  setEmailCodeSameCheck(false);
+  const regex = /^[0-9a-zA-Z]*\@[0-9a-zA-Z]*\.(com|net|co.kr)$/i;
+  if (email !== '' && regex.test(email)) {
+    setEmailValidCheck(true);
+    return true;
+  }
+  setEmailValidCheck(false);
+  return false;
+};
+
+export const isValidLoginPw = ({ setPwValidCheck, pw }: { setPwValidCheck: (pwValidCheck: boolean) => void; pw: string }): boolean => {
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+  if (pw !== '' && regex.test(pw)) {
+    setPwValidCheck(true);
+    return true;
+  }
+  setPwValidCheck(false);
+  return false;
+};
+
+export const isValidSignUpPw = ({
+  setPwValidCheck,
+  setCheckPw,
+  setPwSameCheck,
+  pw,
+}: {
+  setPwValidCheck: (pwValidCheck: boolean) => void;
+  setCheckPw: (checkPw: string) => void;
+  setPwSameCheck: (pwSameCheck: boolean) => void;
+  pw: string;
+}): boolean => {
+  setCheckPw('');
+  setPwSameCheck(false);
   const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
   if (pw !== '' && regex.test(pw)) {
     setPwValidCheck(true);
@@ -24,11 +79,31 @@ export const isValidNickname = ({
 }: {
   setNicknameValidCheck: (pwnNicknameCheck: boolean) => void;
   nickname: string;
-}) => {
+}): boolean => {
   if (nickname !== '') {
     setNicknameValidCheck(true);
     return true;
   }
   setNicknameValidCheck(false);
+  return false;
+};
+
+export const authTokenVerify = (token: string, type: AuthToken): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, type === TOKEN_TYPE.ACCESS ? config.JWT_SECRET : config.JWT_REFRESH_SECRET, (err, decoded) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(decoded);
+    });
+  });
+};
+
+export const isAccessTokenAlive = (): boolean => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    return true;
+  }
   return false;
 };
