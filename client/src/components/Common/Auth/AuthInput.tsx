36b0789d7fdd928756daabdd910bd0118cf36e41
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:48:18 +0900

    [feat] 높이를 추가

--- client/src/components/Common/Auth/AuthInput.tsx
+++ client/src/components/Common/Auth/AuthInput.tsx
@@ -4,12 +4,14 @@ import { Input } from '@/styles/shared';
 
 interface InputProps {
   valid: boolean;
+  isNotExistEmail?: boolean | null;
 }
 
 export const AuthInput = styled(Input)<InputProps>`
   width: 22rem;
+  height: 2rem;
   padding: 5px;
-  font-size: ${(props) => props.theme.size.l};
+  font-size: ${(props) => props.theme.size.m};
   border: 2px solid ${(props) => props.theme.color.black13};
   &:focus {
     outline: none;
@@ -18,7 +20,7 @@ export const AuthInput = styled(Input)<InputProps>`
   }
   color: ${(props) => props.theme.color.black16};
   ${(props) =>
-    !props.valid &&
+    (!props.valid || (props.valid && props.isNotExistEmail === false)) &&
     css`
       border: 2px solid ${props.theme.color.red};
       box-shadow: 0px 0px 6px ${props.theme.color.red};
