321b24f70d5b80642442a9d2f1281c5a3c539f35
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:44:17 +0900

    [feat] 이메일 인증 랜덤 코드를 작성하는 함수를 작성

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -2,3 +2,12 @@ export const verifyRequestData = (arr: any[]): boolean =>
   arr.every((ele) => {
     return ele !== undefined && ele !== null;
   });
+
+export const randomCode = (): string => {
+  const charecters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+  for (let i = 0; i < 8; i += 1) {
+    code += charecters[Math.floor(Math.random() * charecters.length)];
+  }
+  return code;
+};
