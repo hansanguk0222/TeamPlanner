320c3037a7cf8a2f4ad716c320d4801942dbd20c
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 23:43:46 +0900

    [refactor] 200 요청을 보내는 곳 중에서 결과 값을 필요하지 않은 곳을 삭제

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -2,7 +2,7 @@ import passport from 'passport';
 import passportJWT from 'passport-jwt';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { userModel } from '@/models';
-import { ERROR_MESSAGE, OK_MESSAGE } from '@/utils/contants';
+import { ERROR_MESSAGE } from '@/utils/contants';
 import bcrypt from 'bcrypt';
 import config from '@/config';
 
@@ -20,13 +20,11 @@ export default () => {
       async (email, pw, done) => {
         try {
           const [[user]] = await userModel.login({ email });
-          console.log(await bcrypt.hash(pw, 10));
           const isMatch = await bcrypt.compare(pw, user.pw);
-          console.log(isMatch);
           if (!isMatch) {
             return done(null, false, { message: ERROR_MESSAGE.NOT_EXIST_USER });
           }
-          return done(null, user, { message: OK_MESSAGE.LOGIN_SUCCESS });
+          return done(null, user);
         } catch (err) {
           done(err);
         }
