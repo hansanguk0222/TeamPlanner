e651138f18b3ca9d193d3b48ba6677264446604a
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-15 09:02:24 +0900

    [feat] 회원가입에 관한 axios 요청 작업을 추가

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -4,4 +4,13 @@ export const authService = {
   login({ email, pw }: { email: string; pw: string }) {
     return axios.post('/api/auth/login', { email, pw });
   },
+  overlap({ email }: { email: string }) {
+    return axios.post('/api/auth/email/existance', { email });
+  },
+  join({ email, pw, nickname }: { email: string; pw: string; nickname: string }) {
+    return axios.post('/api/auth/sign-up', { email, pw, nickname });
+  },
+  authorizeEmail({ email }: { email: string }) {
+    return axios.post('/api/auth/email/code', { email });
+  },
 };
