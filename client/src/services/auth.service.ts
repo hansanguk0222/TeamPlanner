7a6d3d24a474faa9750eab49b14dffd21a440f2e
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-25 22:58:24 +0900

    [feat] 로그아웃 api 요청을 구현

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
-
 export const authService = {
   login({ email, pw }: { email: string; pw: string }) {
     return axios.post('/api/auth/login', { email, pw });
+  },
+  logout() {
+    return axios.get('/api/auth/logout');
   },
   overlapEmail({ email }: { email: string }) {
     return axios.post('/api/auth/email/join', { email });
