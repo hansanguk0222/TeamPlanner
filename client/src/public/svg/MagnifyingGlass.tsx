fba456e22de86871055aaf594a815d8c202abef0
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-25 22:56:46 +0900

    [feat] 돋보기 그림을 svg로 구현

---
+++ client/src/public/svg/MagnifyingGlass.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { sizeControll } from '@/utils/utils';
+
+export const MagnifyingGlass: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px">
+      <g>
+        <circle cx="15" cy="15" r="5" fill="none" stroke="white" strokeWidth="3px" />
+        <line x1="18" x2="25" y1="18" y2="25" stroke="white" strokeWidth="3px" />
+      </g>
+    </svg>
+  );
+};
