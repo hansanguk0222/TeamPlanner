57aaaebbd9116df1604fbf83dc862cfde463907e
hansanguk0222 <hansanguk0222@gmail.com>
2021-01-30 23:16:32 +0900

    [feat] 특정 팀 아이디에 해당하는 api 작업을 하기 위한 미들웨어 작성

--- server/src/routes/api/teams/index.ts
+++ server/src/routes/api/teams/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import * as teamController from './team.controller';
+import teamIdRouter from './[teamId]';
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', teamController.getTeamList);
-router.get('/users/:userId');
-// router.use('/:teamId');
+router.use('/:teamId', teamIdRouter);
 
 export default router;
