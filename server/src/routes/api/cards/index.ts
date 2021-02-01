dce0cf42faab8f2f0c8e78bdb86c8476b8f2fe4a
hansanguk0222 <hansanguk0222@gmail.com>
2021-02-01 11:30:01 +0900

    [refactor] api 요청 주소 수정

--- server/src/routes/api/cards/index.ts
+++ server/src/routes/api/cards/index.ts
@@ -4,8 +4,8 @@ import cardIdRouter from './[cardId]';
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/cardList/:cardListId', cardController.getCards);
-router.post('/cardList/:cardListId', cardController.createCard);
+router.get('/cardLists/:cardListId', cardController.getCards);
+router.post('/cardLists/:cardListId', cardController.createCard);
 router.use('/:cardId', cardIdRouter);
 
 export default router;
