bfb984293cf4bfa8a0dd198f74b17b8f5a25f2d7
hansanguk0222 <hansanguk0222@gmail.com>
2021-02-01 11:32:47 +0900

    [refactor] 변수명 변경 (teamId -> id)

--- server/src/models/teamModel.ts
+++ server/src/models/teamModel.ts
@@ -2,26 +2,26 @@ import pool from '@/db';
 import { Model } from '@/types';
 
 export const teamModel: Model = {
-  joinTeam({ teamId, channelId }: { teamId: number; channelId: number }) {
-    const sql = `INSERT INTO user_team (teamId, channelId, is_deleted) VALUES (?, ?, ?);`;
-    return pool.execute(sql, [teamId, channelId, false]);
+  joinTeam({ id, channelId }: { id: number; channelId: number }) {
+    const sql = `INSERT INTO user_team (id, channelId, is_deleted) VALUES (?, ?, ?);`;
+    return pool.execute(sql, [id, channelId, false]);
   },
-  withdrawTeam({ teamId, channelId }: { teamId: number; channelId: number }) {
+  withdrawTeam({ id, channelId }: { id: number; channelId: number }) {
     const sql = `UPDATE user_team SET is_deleted=true 
     WHERE team_id=? AND channel_id=?;`;
-    return pool.execute(sql, [teamId, channelId]);
+    return pool.execute(sql, [id, channelId]);
   },
-  updateMoveCnt({ teamId, moveCnt }: { teamId: number; moveCnt: number }) {
+  updateMoveCnt({ id, moveCnt }: { id: number; moveCnt: number }) {
     const sql = 'UPDATE team SET move_cnt=? WHERE id=?;';
-    return pool.execute(sql, [moveCnt, teamId]);
+    return pool.execute(sql, [moveCnt, id]);
   },
-  getTeamUsers({ teamId }: { teamId: number }) {
-    const sql = `SELECT id, email, nickname, profile_image 
+  getTeamUsers({ id }: { id: number }) {
+    const sql = `SELECT id, email, nickname, profile_image AS profileImage
     FROM user
     JOIN user_team
     ON user_team.team_id=? AND user_team.user_id=user.id
     WHERE user_team.is_deleted=0;`;
-    return pool.execute(sql, [teamId]);
+    return pool.execute(sql, [id]);
   },
   getTeamList() {
     const sql = `SELECT id, team_name AS teamName FROM team
