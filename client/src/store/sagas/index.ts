eede99488cda6e5030d58e7a5a37fffd4a30a709
hansanguk0222 <hansanguk0222@gmail.com>
2021-02-01 11:38:25 +0900

    [feat] 유저 사가와 카드 리스트 사가를 추가

--- client/src/store/sagas/index.ts
+++ client/src/store/sagas/index.ts
@@ -2,7 +2,9 @@ import { all, fork } from 'redux-saga/effects';
 import authSaga from './auth.saga';
 import signupSaga from './signup.saga';
 import teamSaga from './team.saga';
+import userSaga from './user.saga';
+import cardListSaga from './cardList.saga';
 
 export default function* rootSaga(): Generator {
-  yield all([fork(authSaga), fork(signupSaga), fork(teamSaga)]);
+  yield all([fork(authSaga), fork(signupSaga), fork(teamSaga), fork(userSaga), fork(cardListSaga)]);
 }
